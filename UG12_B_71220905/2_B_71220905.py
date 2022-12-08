810fa67f72a9fd0906d3760e64e546c377743aca
cexcobain666 <117884180+cexcobain666@users.noreply.github.com>
2022-12-08 12:56:36 +0700

    Add files via upload

--- UG12_B_71220905/2_B_71220905.py
+++ UG12_B_71220905/2_B_71220905.py
@@ -1,21 +1,28 @@
-print("SELAMAT DATANG DI PROGRAM PEPMBUAT PIRAMIDA BERLUBANG")
-bantu=0
-kosong=10
-for i in reversed(range(10)):
-    for j in range(i+1):
-        print(' ', end='')
-    for k in range(bantu+1):
-        if k==0:
-            print('*', end='')
-        else:
-            print(' ', end='')
-    for l in range(bantu):
-        if l==kosong:
-            print('*', end='')
-            kosong+=1
-        else:
-            print(' ', end='')
-    bantu+=1
-    print()
-for i in range(11):
-    print('* ', end='')
+print('======Kalkulator Sederhana=====')
+print('==============================')
+print()
+
+print('1. Penjumlahan')
+print('2. Pengurangan')
+print('3. Perkalian')
+print('4. Pembagian')
+print('5. Modulus')
+print()
+
+pilihan = int(input('Input pilihan operasi: '))
+num1 = float(input('Angka pertama: '))
+num2 = float(input('Angka kedua: '))
+print()
+
+if pilihan == 1:
+  print('Hasil dari',num1,'+',num2,'=',round(num1+num2,2))
+elif pilihan == 2:
+  print('Hasil dari',num1,'-',num2,'=',round(num1-num2,2))
+elif pilihan == 3:
+  print('Hasil dari',num1,'*',num2,'=',round(num1*num2,2))
+elif pilihan == 4:
+  print('Hasil dari',num1,'/',num2,'=',round(num1/num2,2))
+elif pilihan == 5:
+  print('Hasil dari',num1,'%',num2,'=',round(num1%num2,2))
+else:
+  print('sorry diks,  menu tidak tersedia')
