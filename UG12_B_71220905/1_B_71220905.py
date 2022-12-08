20a3cdb440eb1195991503843fe7aee559105476
cexcobain666 <117884180+cexcobain666@users.noreply.github.com>
2022-12-08 12:53:40 +0700

    Add files via upload

---
+++ UG12_B_71220905/1_B_71220905.py
@@ -0,0 +1,21 @@
+print("SELAMAT DATANG DI PROGRAM PEPMBUAT PIRAMIDA BERLUBANG")
+bantu=0
+kosong=10
+for i in reversed(range(10)):
+    for j in range(i+1):
+        print(' ', end='')
+    for k in range(bantu+1):
+        if k==0:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    for l in range(bantu):
+        if l==kosong:
+            print('*', end='')
+            kosong+=1
+        else:
+            print(' ', end='')
+    bantu+=1
+    print()
+for i in range(11):
+    print('* ', end='')
